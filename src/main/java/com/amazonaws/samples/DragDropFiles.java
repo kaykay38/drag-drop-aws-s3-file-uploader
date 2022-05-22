559d7d71475efa7888495ca45d4159a59aa0e2c0
tyL0905 <alexliaovv@gmail.com>
2022-05-21 19:25:20 -0700

    download file proto-type

--- src/main/java/com/amazonaws/samples/DragDropFiles.java
+++ src/main/java/com/amazonaws/samples/DragDropFiles.java
@@ -38,6 +38,7 @@ import com.amazonaws.services.s3.model.ListObjectsRequest;
 import com.amazonaws.services.s3.model.ObjectListing;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
 
 public class DragDropFiles extends JFrame {
@@ -76,6 +77,7 @@ public class DragDropFiles extends JFrame {
 			// create a parent node
 			String bucketName = bucket.getName();
 			parent = new DefaultMutableTreeNode(bucketName);
+			parent.
 			// add the node to the tree
 			root.add(parent);
 
@@ -266,6 +268,12 @@ public class DragDropFiles extends JFrame {
 				if (downloadPath != null) {
 					JOptionPane.showMessageDialog(null,
 							"You like to downloand a file from cloud from buckets:" + downloadPath.toString());
+					System.out.println(downloadPath);
+					
+					String bucketName = downloadPath.getPathComponent(1).toString();
+					String key = downloadPath.getPathComponent(2).toString();
+					
+					downloadObjByKey(bucketName, key);
 				}
 			}
 		});
@@ -300,6 +308,33 @@ public class DragDropFiles extends JFrame {
 		test.pack();
 		test.setVisible(true);
 	}
+	
+	private static void downloadObjByKey(String buketName, String key){
+       
+        try {
+            S3Object o = s3.getObject(buketName, key);
+            S3ObjectInputStream s3is = o.getObjectContent();
+            
+            FileOutputStream fos = new FileOutputStream(new File("C:\\Users\\zh_lt\\Desktop\\hhh_download.txt"));
+            byte[] read_buf = new byte[1024];
+            int read_len = 0;
+            while ((read_len = s3is.read(read_buf)) > 0) {
+                fos.write(read_buf, 0, read_len);
+            }
+            s3is.close();
+            fos.close();
+        } catch (AmazonServiceException e) {
+            System.err.println(e.getErrorMessage());
+            System.exit(1);
+        } catch (FileNotFoundException e) {
+            System.err.println(e.getMessage());
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }
+    }
+
 
 	private void copyFile(File source, File dest) throws IOException {
 		InputStream input = null;
